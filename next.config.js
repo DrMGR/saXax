5dd051e18e26187370ce8db55c7ea26c16da7e04
Elavarasan <elavarasa.003@gmail.com>
2023-08-10 16:40:28 +0530

    just update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // Other config options...
-    output: "standalone", // Generate static HTML files for each route
-    exportPathMap: async function () {
-        // Your custom exportPathMap logic here
-    },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
