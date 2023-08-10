b5829ba3584e7421e17b9bc3b797c59ad56a49d6
Elavarasan <elavarasa.003@gmail.com>
2023-08-10 16:03:42 +0530

    just update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+
+module.exports = {
+    nextConfig,
+    // Other config options...
+    output: {
+        // Generate static HTML files for each route
+        export: true,
+    },
+};
